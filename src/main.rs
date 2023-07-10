8c41a220c9e335e944f0098958cb38fd67f71ea7
Alexander Schmolck <a.schmolck+git@gmail.com>
2023-07-10 16:20:37 +0100

    ? it.

--- src/main.rs
+++ src/main.rs
@@ -9,46 +9,46 @@ fn column_ruler(mut col: usize) -> String {
     }
     return format!(" {}", SUP[(col % 10) as usize]);
 }
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut stdout = io::stdout().lock();
     let mut octets: usize = 0;
     for (i, line) in io::stdin().lines().enumerate() {
         let ln = i + 1;
-        let line = line.expect("Error reading STDIN");
+        let line = line?;
         let line = line.as_bytes();
-        write!(stdout, "\x1b[32m{ln:6}\x1b[39m\x1b[1m\x1b[91m");
+        write!(stdout, "\x1b[32m{ln:6}\x1b[39m\x1b[1m\x1b[91m")?;
         for c in line {
             match c {
                 33..=126 => {
-                    write!(stdout, "{}", char::from_u32(*c as u32 + FW_OFFSET).unwrap());
+                    write!(stdout, "{}", char::from_u32(*c as u32 + FW_OFFSET).unwrap())?;
                 }
                 32 => {
-                    write!(stdout, "  ");
+                    write!(stdout, "  ")?;
                 }
                 127 => {
-                    write!(stdout, "^?");
+                    write!(stdout, "^?")?;
                 }
                 0..=31 => {
-                    write!(stdout, "\x1b[7m^{}\x1b[27m", ((*c as u8) + 64) as char);
+                    write!(stdout, "\x1b[7m^{}\x1b[27m", ((*c as u8) + 64) as char)?;
                 }
                 _ => {
-                    write!(stdout, "\x1b[7m{c:2x}\x1b[27m");
+                    write!(stdout, "\x1b[7m{c:2x}\x1b[27m")?;
 
                     // panic!("I'm gonna deal with that later");
                 }
             }
         }
-        write!(stdout, "\x1b[m\n");
-        write!(stdout, "\x1b[m{octets:06x}\x1b[m");
+        write!(stdout, "\x1b[m\n")?;
+        write!(stdout, "\x1b[m{octets:06x}\x1b[m")?;
         for (j, c) in line.iter().enumerate() {
             write!(
                 stdout,
                 "{}{c:02x}\x1b[m",
                 ["\x1b[22m", "\x1b[2m"][(j + octets) % 2]
-            );
+            )?;
         }
-        write!(stdout, "\x1b[m\n");
-        write!(stdout, "\x1b[31m{octets:6}\x1b[m");
+        write!(stdout, "\x1b[m\n")?;
+        write!(stdout, "\x1b[31m{octets:6}\x1b[m")?;
         for (j, _) in line.iter().enumerate() {
             let divisible_by_10 = (j % 10 == 0) as usize;
             write!(
@@ -56,9 +56,10 @@ fn main() {
                 "{}{}",
                 ["\x1b[2m", "\x1b[22m"][divisible_by_10],
                 column_ruler(j),
-            );
+            )?;
         }
-        write!(stdout, "\x1b[m\n");
+        write!(stdout, "\x1b[m\n")?;
         octets += line.len();
     }
+    Ok(())
 }
